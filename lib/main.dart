85bc31fac9a824121c4e3b3ab55d0564c67f1548
Nidhin Pc <nidhinpc66666@gmail.com>
2025-07-30 12:20:12 +0530

    Merge branch 'review' of https://github.com/nidhinpc/erguo_application into review

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,15 @@ import 'package:erguo/view/worker_register.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Supabase.initialize(
+    url: 'https://hzyfzaxdfunskijhlnfu.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh6eWZ6YXhkZnVuc2tpamhsbmZ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM3NzA1MzIsImV4cCI6MjA2OTM0NjUzMn0.Ma7XCXRVDOzpHNQHg-1cUx4fQTYZ7XLF0FVrkXM6Zjg',
+  );
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   await FirebaseAuth.instance.signInAnonymously();
